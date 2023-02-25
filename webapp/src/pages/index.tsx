36b20d9190e719c01d8eb4e4b595f6771255fab8
Jingcheng Wang <wjcauskew@hotmail.com>
2023-02-25 11:50:33 +1100

    Display temp and time on home page

--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -1,20 +1,24 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import useSWR from "swr";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  //TODO make this fetcher generic
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  //TODO make a separate layer for data fetching
+  const { data } = useSWR("/api/current-temp", fetcher);
+
   return (
     <>
       <Head>
         <title>Room Temperature Monitor</title>
-        <meta name="description" content="This is a new analyser app" />
+        <meta name="description" content="This is a web app to monitor my room temperature" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="text-3xl">
-        <p>Front end under construction</p>
+      <main>
+        <h1 className="text-center text-2xl font-bold mt-6">The current room temp is {data?.temp} &#8451; <br/> at {data?.timeRecored}</h1>
       </main>
     </>
   )
