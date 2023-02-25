1d34195144863b82c6aaea37038efdad93781754
Jingcheng Wang <wjcauskew@hotmail.com>
2023-02-25 11:29:09 +1100

    Add favicon;Set up tailwind

--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -9,12 +8,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>News Analyser</title>
+        <title>Room Temperature Monitor</title>
         <meta name="description" content="This is a new analyser app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className="text-3xl">
         <p>Front end under construction</p>
       </main>
     </>
